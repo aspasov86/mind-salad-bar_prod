653c7da814ffa8a05fbc2c6f4b487e0cd613ff44
unknown <acko1986@yahoo.com>
2020-12-18 01:06:09 +0100

    fetch link changed

--- src/services/ingredientService.js
+++ src/services/ingredientService.js
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
 export const getIngredients = async () => {
-  const res = await axios.get('/ingredients');
+  const res = await axios.get('https://5fca00983c1c220016441782.mockapi.io/api/v1/ingredients');
   return res.data;
 };
 
 export const getIngredientById = async (id) => {
-  const res = await axios.get(`/ingredients/${id}`);
+  const res = await axios.get(`https://5fca00983c1c220016441782.mockapi.io/api/v1/ingredients/${id}`);
   return res.data;
 };
 
 export const createIngredient = async (data) => {
-  const res = await axios.post('/ingredients', data);
+  const res = await axios.post('https://5fca00983c1c220016441782.mockapi.io/api/v1/ingredients', data);
   return res.data;
 };
 
 export const updateIngredient = async (id, data) => {
-  const res = await axios.put(`/ingredients/${id}`, data);
+  const res = await axios.put(`https://5fca00983c1c220016441782.mockapi.io/api/v1/ingredients/${id}`, data);
   return res.data;
 };
 
 export const deleteIngredient = async (id) => {
-  const res = await axios.delete(`/ingredients/${id}`);
+  const res = await axios.delete(`https://5fca00983c1c220016441782.mockapi.io/api/v1/ingredients/${id}`);
   return res.data;
 };
--- src/services/saladService.js
+++ src/services/saladService.js
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
 export const getSalads = async () => {
-  const res = await axios.get('/salads');
+  const res = await axios.get('https://5fca00983c1c220016441782.mockapi.io/api/v1/salads');
   return res.data;
 };
 
 export const getSaladById = async (id) => {
-  const res = await axios.get(`/salads/${id}`);
+  const res = await axios.get(`https://5fca00983c1c220016441782.mockapi.io/api/v1/salads/${id}`);
   return res.data;
 };
 
 export const createSalad = async (data) => {
-  const res = await axios.post('/salads', data);
+  const res = await axios.post('https://5fca00983c1c220016441782.mockapi.io/api/v1/salads', data);
   return res.data;
 };
 
 export const updateSalad = async ({ id, ...data }) => {
-  const res = await axios.put(`/salads/${id}`, data);
+  const res = await axios.put(`https://5fca00983c1c220016441782.mockapi.io/api/v1/salads/${id}`, data);
   return res.data;
 };
 
 export const deleteSalad = async (id) => {
-  const res = await axios.delete(`/salads/${id}`);
+  const res = await axios.delete(`https://5fca00983c1c220016441782.mockapi.io/api/v1/salads/${id}`);
   return res.data;
 };
